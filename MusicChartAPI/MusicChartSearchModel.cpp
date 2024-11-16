254b522cd8557a11da1acceeae33a3a547e7140f
shinjeongmin <sin37minduke@naver.com>
2024-11-17 00:43:48 +0900

    feat: implement music chart model.

--- MusicChartAPI/MusicChartSearchModel.cpp
+++ MusicChartAPI/MusicChartSearchModel.cpp
@@ -1,5 +1,113 @@
 #include "MusicChartSearchModel.h"
 
 MusicChartSearchModel::MusicChartSearchModel(QObject *parent)
-    : QObject{parent}
-{}
+    : QAbstractListModel{parent}
+{
+    m_names[MusicName] = "musicName";
+    m_names[MusicSinger] = "musicSinger";
+    m_names[MusicImageSource] = "musicImageSource";
+}
+
+int MusicChartSearchModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return m_musicList.size();
+}
+
+QVariant MusicChartSearchModel::data(const QModelIndex &index, int role) const
+{
+    if(index.isValid() && index.row() >= 0 && index.row() < m_musicList.size()){
+        MusicItemData* musicItem = m_musicList[index.row()];
+
+        switch(role){
+        case MusicName:
+            return musicItem->title();
+        case MusicSinger:
+            return musicItem->singerName();
+        case MusicImageSource:
+            return musicItem->imageSource();
+        default:
+            return {};
+        }
+    }
+
+    return {};
+}
+
+QHash<int, QByteArray> MusicChartSearchModel::roleNames() const
+{
+    return m_names;
+}
+
+bool MusicChartSearchModel::isSearching() const
+{
+    return m_isSearching;
+}
+
+void MusicChartSearchModel::setIsSearching(bool newIsSearching)
+{
+    if (m_isSearching == newIsSearching)
+        return;
+    m_isSearching = newIsSearching;
+    emit isSearchingChanged();
+}
+
+void MusicChartSearchModel::search(const QString &name)
+{
+    QUrlQuery query;
+    QString songName = name;
+
+    songName.replace(" ", "+");
+    query.addQueryItem("term", songName);
+    query.addQueryItem("limit", "25");
+
+    qDebug() << "query:" << requestDefaultUrl + "?" + query.toString();
+
+    setIsSearching(true);
+    m_reply = m_networkManager.get(QNetworkRequest(requestDefaultUrl + "?" + query.toString()));
+    connect(m_reply, &QNetworkReply::finished, this, &MusicChartSearchModel::parseData);
+}
+
+void MusicChartSearchModel::parseData()
+{
+    if(m_reply->error() == QNetworkReply::NoError){
+        beginResetModel();
+
+        if(!m_musicList.empty()){
+            qDeleteAll(m_musicList);
+            m_musicList.clear();
+        }
+
+        QByteArray data = m_reply->readAll();
+
+        QJsonDocument jsonDocument = QJsonDocument::fromJson(data);
+        int resultCount = jsonDocument["resultCount"].toInt();
+
+        if(resultCount > 0){
+            QJsonArray results = jsonDocument["results"].toArray();
+
+            for(const auto &result : results){
+                QJsonObject item = result.toObject();
+
+                MusicItemData* musicItem = new MusicItemData(this);
+
+                musicItem->setTitle(item["trackName"].toString());
+                musicItem->setSingerName(item["artistName"].toString());
+                musicItem->setImageSource(item["artworkUrl100"].toString());
+
+                m_musicList << musicItem;
+            }
+        }
+        else{
+            // Empty
+        }
+
+        endResetModel();
+    } else {
+        // Error!
+        qDebug() << "Error:" << m_reply->errorString();
+    }
+
+    setIsSearching(false);
+    m_reply->deleteLater();
+}
--- MusicChartAPI/MusicChartSearchModel.h
+++ MusicChartAPI/MusicChartSearchModel.h
@@ -1,17 +1,59 @@
 #ifndef MUSICCHARTSEARCHMODEL_H
 #define MUSICCHARTSEARCHMODEL_H
 
-#include <QObject>
+#include <QAbstractListModel>
 #include <QQmlEngine>
+#include <QNetworkAccessManager>
+#include "MusicItemData.h"
 
-class MusicChartSearchModel : public QObject
+#include <QUrlQuery>
+#include <QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QString>
+
+#include <QDebug>
+
+class MusicItemData;
+
+class MusicChartSearchModel : public QAbstractListModel
 {
     Q_OBJECT
     QML_ELEMENT
+    Q_PROPERTY(bool isSearching READ isSearching  WRITE setisSearching  NOTIFY isSearchingChanged FINAL)
+
 public:
+    enum Role{
+        MusicName = Qt::UserRole + 1,
+        MusicSinger,
+        MusicImageSource,
+    };
+
     explicit MusicChartSearchModel(QObject *parent = nullptr);
 
+    virtual int rowCount(const QModelIndex &parent) const override;
+    virtual QVariant data(const QModelIndex &index, int role) const override;
+    virtual QHash<int, QByteArray> roleNames() const override;
+
+    bool isSearching() const;
+    void setIsSearching(bool newIsSearching);
+
+public slots:
+    void search(const QString &name);
+    void parseData();
+
 signals:
+    void isSearchingChanged();
+private:
+    QHash<int, QByteArray> m_names;
+
+    const QString requestDefaultUrl = "https://itunes.apple.com/search";
+    QNetworkAccessManager m_networkManager;
+    QNetworkReply *m_reply = nullptr;
+
+    QList<MusicItemData*> m_musicList;
+    bool m_isSearching;
 };
 
 #endif // MUSICCHARTSEARCHMODEL_H
